4375766b5156c9b53b3b1c80847ef7e057657937
eunbinee14 <bhks0412@naver.com>
2023-12-04 23:39:34 +0900

    visual

---
+++ visual_inflict_car.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+from matplotlib import font_manager, rc
+import pandas as pd
+import seaborn as sns
+
+# 한글 폰트 설정
+font_path = "C:/Windows/Fonts/malgun.ttf"
+font_name = font_manager.FontProperties(fname=font_path).get_name()
+rc('font', family=font_name)
+
+data = pd.read_csv('전처리후데이터.csv')
+
+## 원핫인코딩으로 전처리한 데이터셋으로 카테고리(가해차종) 별로 사고 횟수가 얼만큼인지 시각적으로 파악 ##
+
+
+# 각 카테고리별로 사고 횟수
+count_by_vehicle_type = data.filter(like='가해차종').sum()
+
+# bar 그래프로 시각화
+plt.figure(figsize=(10, 5))
+plt.bar(count_by_vehicle_type.index, count_by_vehicle_type)
+
+# X축 눈금 간격 조정 - 사이 간격
+plt.xticks(rotation=45)
+plt.xticks(range(0, len(count_by_vehicle_type), 2), count_by_vehicle_type.index[::2])
+
+plt.title('가해차종별 사고 횟수')
+plt.xlabel('가해차종')
+plt.ylabel('사고 횟수')
+plt.tight_layout()
+plt.show()
