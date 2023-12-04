7639266507b97a1c9e3c235db4d3d50416292fa0
eunbinee14 <bhks0412@naver.com>
2023-12-04 22:46:45 +0900

    visualization - time of accidnet count

--- raw_data.py
+++ raw_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-raw_data=pd.read_csv('2015_2020.csv',encoding='cp949')
+raw_data=pd.read_csv('2015_2020.csv', encoding='cp949')
 
 # 기존 캡스톤 때 진행했던 2015-2017년 데이터와 새로 생긴 2018-2020 데이터를 합치면서 데이터 값의 일관성이 꺠진 부분 발생
 # 사고등급 열에 어떤 것은 중상사고로 입력 되어있고 어떤 것은 중상으로 입력되어있음
