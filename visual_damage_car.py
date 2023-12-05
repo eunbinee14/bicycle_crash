cb73f3d229b4bd075251d28ab526e4e586fec8cb
eunbinee14 <bhks0412@naver.com>
2023-12-05 13:22:58 +0900

    visual

--- visual_damage_car.py
+++ visual_damage_car.py
@@ -18,11 +18,15 @@ count_by_vehicle_type = data.filter(like='피해차종').sum()
 
 # bar 그래프로 시각화
 plt.figure(figsize=(10, 5))
-plt.bar(count_by_vehicle_type.index, count_by_vehicle_type)
+bars = plt.bar(count_by_vehicle_type.index, count_by_vehicle_type)
 
 # X축 눈금 간격 조정 - 사이 간격
-plt.xticks(rotation=45)
-plt.xticks(range(0, len(count_by_vehicle_type), 2), count_by_vehicle_type.index[::2])
+plt.xticks(rotation=45, ha='right')  # 레이블을 읽기 쉽게 회전
+plt.xticks(range(len(count_by_vehicle_type)), count_by_vehicle_type.index)  # x-tick 위치 조정
+
+# 막대 위에 카운트 값을 표시
+for bar, count in zip(bars, count_by_vehicle_type):
+    plt.text(bar.get_x() + bar.get_width() / 2 - 0.1, bar.get_height() + 0.1, str(count), fontsize=9)
 
 plt.title('피해차종별 사고 횟수')
 plt.xlabel('피해차종')
